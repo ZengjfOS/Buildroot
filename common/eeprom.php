ae0d766bbcc2a840e511f56d75b07942ab12a1c0
zengjianfeng <zengjianfeng@aplex.com.tw>
2018-03-06 13:31:32 +0800

    fix eeprom data transfer error.

--- common/eeprom.php
+++ common/eeprom.php
@@ -1,11 +1,38 @@
 <?php
+	/**
+	 * get eeprom file data: dd if=/sys/bus/i2c/devices/3-0050/eeprom of=file
+	 * vim open file and execute show hex format: `:%!xxd`
+	 * get data use php:
+	 *     <?php
+	 *     	$eepromfile = fopen("/sys/bus/i2c/devices/3-0050/eeprom", "r+") or die("Unable to open file!");
+	 *     	print_r(unpack('C*', fgets($eepromfile)));
+	 *     	fclose($eepromfile);
+	 *     ?>
+ 	 */
 	if ($_GET["version"] == 3) {
 		$bin_str .= pack('C', 0x01);
 		$bin_str .= pack('C', 0x06);
 
 		$eepromfile = fopen("/sys/bus/i2c/devices/3-0050/eeprom", "w+") or die("Unable to open file!");
 		for ($x=0; $x<6; $x++) {
-			$bin_str .= pack('C', ((int)($_GET["mac"][2 * $x]) * 16 + (int)($_GET["mac"][2 * $x + 1])));
+			$byteval = 0;
+			for ($j=0; $j<2; $j++) {
+				$hexval = 0;
+				$charval = ord($_GET["mac"][2 * $x + $j]);
+				if ($charval >= ord('0') && $charval <= ord('9'))
+					$hexval = $charval - ord('0');
+				if ($charval >= ord('a') && $charval <= ord('z'))
+					$hexval = $charval - ord('a') + 10;
+				if ($charval >= ord('A') && $charval <= ord('Z'))
+					$hexval = $charval - ord('A') + 10;
+
+				if (($j % 2) == 0)
+					$hexval *= 16;
+
+				$byteval += $hexval;
+			}
+
+			$bin_str .= pack('C', $byteval);
 		} 
 
 		for ($x=0; $x<(256 - 8 - 2); $x++) {
---
+++ docs/004_EEPROM_Data.md
@@ -0,0 +1,10 @@
+# EEPROM Data
+
+## refer
+
+* [Data Format](refers/EEPROM_V30807.docx)
+
+## Source Code
+
+* [../common/eeprom.php](../common/eeprom.php)
+
